75427b8c871b8dc8edcb4979bc6436fa58195aaa
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-04 13:57:46 +0900

    docker setting hot-reload uses ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import express from 'express';
+const app: express.Express = express();
 
-console.log(hello('TypeScript'));
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
